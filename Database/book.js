076964592e6a61dfdb5b7eb2f5b38296a04b7c0f
Jyothi N <jyothin2903@gmail.com>
2021-07-05 16:07:56 +0530

    microservices

--- Database/book.js
+++ Database/book.js
@@ -1,19 +1,28 @@
 const mongoose = require("mongoose");
 
-//Creating a book schema
+// Creating a book schema
 const BookSchema = mongoose.Schema({
-        ISBN : String,
-        title :  String,
-        authors : [Number],
-        language :  String,
-        pubDate :  String,
-        numOfPage : Number,
-        category : [ String],
-        publication : Number,
-    
+  ISBN: {
+    type: String,
+    required: true,
+    minLength: 8,
+    maxLength: 10,
+  }, // required
+  title: {
+    type: String,
+    required: true,
+    minLength: 8,
+    maxLength: 10,
+  },
+  authors: [Number],
+  language: String,
+  pubDate: String,
+  numOfPage: Number,
+  category: [String],
+  publication: Number,
 });
 
-//Create a book model
-const BookModel = mongoose.model("books",BookSchema);
+// Create a book model
+const BookModel = mongoose.model("books", BookSchema);
 
 module.exports = BookModel;
